372cc42f5113fd1f753ba35d38f70a29d9e32fe9
Hélio <helinho.le@gmail.com>
2026-08-06 10:28:52 -0300

    desserialização manual - apenas para estudos

--- src/main/java/com/consultacep/services/ConsultaCEPClient.java
+++ src/main/java/com/consultacep/services/ConsultaCEPClient.java
@@ -5,12 +5,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class ConsultaCEPClient {
 
   Gson gson = new Gson();
-  Endereco endereco = new Endereco();
-
+ 
   public void ConsultaClient(String cep) throws Exception {
     HttpClient client = HttpClient.newHttpClient();
 
@@ -21,17 +22,22 @@ public class ConsultaCEPClient {
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
      JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
-     String logadouro = json.get("logadouro").getAsString();
-     
+
+      String logadouro = json.get("logradouro").getAsString();
+      String bairro = json.get("bairro").getAsString();
+      String localidade = json.get("localidade").getAsString();
+      String uf = json.get("uf").getAsString();
+      String regiao = json.get("regiao").getAsString();
+      String ddd = json.get("ddd").getAsString();
 
     System.out.println("========== RELATÓRIO ==========");
     System.out.println("CEP: " + cep);
-    System.out.println("Logadouro: " 
-    System.out.println("Bairro: " + 
-    System.out.println("Localidade: " + 
-    System.out.println("UF: " + 
-    System.out.println("Região: " + 
-    System.out.println("DDD: " + 
+    System.out.println("Logadouro: " + logadouro); 
+    System.out.println("Bairro: " + bairro);
+    System.out.println("Localidade: " + localidade);
+    System.out.println("UF: " + uf);
+    System.out.println("Região: " + regiao);
+    System.out.println("DDD: " + ddd);
     System.out.println("===============================");
 
   }
